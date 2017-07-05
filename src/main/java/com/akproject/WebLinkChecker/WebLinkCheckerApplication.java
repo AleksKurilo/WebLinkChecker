ed96abd9ed688db4a619e8c46b69e3cea1c77a61
AleksKurilo <aleks_prime@mail.ru>
2017-07-05 11:08:21 +0300

    add

--- src/main/java/com/akproject/WebLinkChecker/WebLinkCheckerApplication.java
+++ src/main/java/com/akproject/WebLinkChecker/WebLinkCheckerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebLinkCheckerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WebLinkCheckerApplication.class, args);
 	}
 }
