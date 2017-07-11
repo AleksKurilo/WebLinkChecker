e27fc315378faaea3600bb87b7c93c29aa4a0bf5
AleksKurilo <aleks_prime@mail.ru>
2017-07-11 13:18:10 +0300

    Test use connection to the database

--- src/main/java/com/akproject/WebLinkChecker/WebLinkCheckerApplication.java
+++ src/main/java/com/akproject/WebLinkChecker/WebLinkCheckerApplication.java
@@ -1,14 +1,40 @@
 package com.akproject.WebLinkChecker;
 
+
+import models.User;
+import models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class WebLinkCheckerApplication {
+/*
+	@Autowired
+	private UserRepository userRepository;
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			User user = new User("aleks");
+			user.setName("aleks");
+			userRepository.save(user);
+		};
+	}
+*/
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(WebLinkCheckerApplication.class, args);
+	//	SpringApplication.run(WebLinkCheckerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(WebLinkCheckerApplication.class);
+		UserRepository repository = (UserRepository)context.getBean(UserRepository.class);
+
+		repository.save(new User("aleks"));
+
 
 
 	}
