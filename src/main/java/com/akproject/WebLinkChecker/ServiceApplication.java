f84651481cd8db97a735c2929148942c243a0eff
AleksKurilo <aleks_prime@mail.ru>
2017-07-24 15:04:16 +0300

    Connect the plug-in lombok

--- src/main/java/com/akproject/WebLinkChecker/ServiceApplication.java
+++ src/main/java/com/akproject/WebLinkChecker/ServiceApplication.java
@@ -2,24 +2,30 @@ package com.akproject.WebLinkChecker;
 
 import com.akproject.WebLinkChecker.model.Project;
 import com.akproject.WebLinkChecker.model.ProjectRepository;
+import com.akproject.WebLinkChecker.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 
 @SpringBootApplication
 public class ServiceApplication {
 
 	@Autowired
-	private ProjectRepository projectRepository;
+	private ProjectService projectService;
 
 	@Bean
 	public CommandLineRunner commandLineRunner(){
 		return args -> {
-			Project project = new Project("diss");
-			projectRepository.save(project);
+			//Project project = new Project("test2");
+			//projectService.addProject(project);
+
+			Iterable<Project> list = projectService.findAll();
+
 		};
 	}
 	public static void main(String[] args) {
--- src/main/java/com/akproject/WebLinkChecker/model/ProjectRepository.java
+++ src/main/java/com/akproject/WebLinkChecker/model/ProjectRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
     List<Project> findByName(String name);
+
 }
