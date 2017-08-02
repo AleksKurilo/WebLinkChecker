9d36705a67e6a70951a1686a418133246e3932f2
AleksKurilo <aleks_prime@mail.ru>
2017-08-02 11:11:13 +0300

    The page for saving a new project in database

--- src/main/java/com/akproject/WebLinkChecker/controllers/ProjectController.java
+++ src/main/java/com/akproject/WebLinkChecker/controllers/ProjectController.java
@@ -5,7 +5,6 @@ import com.akproject.WebLinkChecker.service.ProjectService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
