b77a6706605ea4ef3c7b01f5f493f793f5e81a25
AleksKurilo <aleks_prime@mail.ru>
2017-10-31 13:56:30 +0200

    Clean code

--- src/main/resources/static/js/ajax-delete.js
+++ src/main/resources/static/js/ajax-delete.js
@@ -1,23 +1,28 @@
-$(document).ready(function() {
-    $(document).on("click", ".remove-project", function (){
-        if (confirm('Are you sure you want to delete this?')) {
-            var rowToDelete = $(event.target).closest("tr");
-            $.ajax({
-                url: $(this).attr("href"),
-                type: "DELETE",
-                dataType: "text",
-                success: function (response) {
-                    rowToDelete.remove();
-                    var respContent = "";
-                    respContent += "<div class=\"alert alert-success\">" +
-                        "<span class='success'><strong>Success!</strong> Project was delete</span></div>";
-                    $("#formResponse").html(respContent);
-                },
-                error: function (e) {
-                    alert("Project doesn't found" + e);
+$(document).ready(function () {
+    $(document).on("click", ".remove-project", function () {
+        var rowToDelete = $(event.target).closest("tr");
+        $.ajax({
+            beforeSend: function (request) {
+                if (confirm('Are you sure you want to delete this?')) {
                 }
-            });
-            return false;
-        }
+                else {
+                    return false;
+                }
+            },
+            url: $(this).attr("href"),
+            type: "DELETE",
+            dataType: "text",
+            success: function (response) {
+                rowToDelete.remove();
+                var respContent = "";
+                respContent += "<div class=\"alert alert-success\">" +
+                    "<span class='success'><strong>Success!</strong> Project was delete</span></div>";
+                $("#formResponse").html(respContent);
+            },
+            error: function (e) {
+                alert("Project doesn't found" + e);
+            }
+        });
+        return false;
     });
 });
