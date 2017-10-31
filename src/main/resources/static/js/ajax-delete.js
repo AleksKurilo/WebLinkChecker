af3f2449a0e232023dcee271c91e0af4fa546865
AleksKurilo <aleks_prime@mail.ru>
2017-10-31 11:31:11 +0200

    Clean code

--- src/main/resources/static/js/ajax-delete.js
+++ src/main/resources/static/js/ajax-delete.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
     $(document).on("click", ".remove-project", function (){
         if (confirm('Are you sure you want to delete this?')) {
-        var rowToDelete = $(event.target).closest("tr");
-        $.ajax({
-            url: $(this).attr("href"),
-            type: "DELETE",
-            dataType: "text",
-            success: function (response) {
-                rowToDelete.remove();
-                var respContent = "";
-                respContent += "<div class=\"alert alert-success\">" +
-                    "<span class='success'><strong>Success!</strong> Project was delete</span></div>";
-                $("#formResponse").html(respContent);
-            },
-            error: function (e) {
-                alert("Project doesn't found" + e);
-            }
-        });
-        return false;
-    }
+            var rowToDelete = $(event.target).closest("tr");
+            $.ajax({
+                url: $(this).attr("href"),
+                type: "DELETE",
+                dataType: "text",
+                success: function (response) {
+                    rowToDelete.remove();
+                    var respContent = "";
+                    respContent += "<div class=\"alert alert-success\">" +
+                        "<span class='success'><strong>Success!</strong> Project was delete</span></div>";
+                    $("#formResponse").html(respContent);
+                },
+                error: function (e) {
+                    alert("Project doesn't found" + e);
+                }
+            });
+            return false;
+        }
     });
 });
