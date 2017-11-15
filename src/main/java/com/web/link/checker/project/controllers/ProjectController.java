75a09f0a4a2168e7807f212b3d962f38c3d5b4e9
aleksk <aleks_prime@mail.ru>
2017-11-15 08:43:33 +0200

    test

--- src/main/java/com/web/link/checker/project/controllers/ProjectController.java
+++ src/main/java/com/web/link/checker/project/controllers/ProjectController.java
@@ -9,13 +9,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.List;
-
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static com.web.link.checker.project.controllers.ProjectBinding.*;
 
@@ -77,7 +79,7 @@ public class ProjectController {
 
     @RequestMapping(path = DELETE, method = RequestMethod.GET)
     public String delete(@PathVariable String uuid) {
-        projectFacade.delete(uuid);
+        System.out.println("Hello Dima");
         return "redirect:" + BASE_PATH;
     }
 
